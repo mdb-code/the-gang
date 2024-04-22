225c34cb750ebf7e8b6c6847b1d4cb431edf9c9d
mdb-code <mihalydomokosbarna@gmail.com>
2024-04-22 10:44:57 +0200

    Owned it! Done!
    Mostly done

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('Hey there!')
+
+
+function showPopup () {
+    var popup = document.getElementById('popup')
+    popup.classList.add('show')
+}
+
+function hidePopup () {
+    var popup = document.getElementById('popup')
+    popup.classList.remove('show')
+}
